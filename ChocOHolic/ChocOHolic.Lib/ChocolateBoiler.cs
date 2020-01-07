57fdc771d8a2c2d5a49584d05e1c1a8b6adc1a00
Aram Zhamkochyan <aramzham@hotmail.com>
2020-01-07 17:45:48 +0400

    non singleton chocolate boiler

---
+++ ChocOHolic/ChocOHolic.Lib/ChocolateBoiler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ChocOHolic.Lib
+{
+    public class ChocolateBoiler
+    {
+        public bool IsEmpty { get; private set; }
+        public bool IsBoiled { get; private set; }
+
+        public ChocolateBoiler()
+        {
+            IsEmpty = true;
+            IsBoiled = false;
+        }
+
+        public void Fill()
+        {
+            if (IsEmpty)
+            {
+                IsEmpty = false;
+                IsBoiled = false;
+                // fill the boiler with a milk/chocolate mixture
+            }
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty && IsBoiled)
+            {
+                // drain the boiled milk and chocolate
+                IsEmpty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty && !IsBoiled) {
+                // bring the contents to a boil
+                IsBoiled = true;
+            }
+        }
+    }
+}
