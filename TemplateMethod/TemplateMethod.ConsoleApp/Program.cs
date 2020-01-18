aedf812bc38b1a7e4bac00585c16426ce99a6299
Aram Zhamkochyan <aramzham@hotmail.com>
2020-01-18 13:36:46 +0400

    duck sorting with Array class

--- TemplateMethod/TemplateMethod.ConsoleApp/Program.cs
+++ TemplateMethod/TemplateMethod.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TemplateMethod.Lib;
 using TemplateMethod.Lib.Concretes;
 
 namespace TemplateMethod.ConsoleApp
@@ -18,7 +19,33 @@ namespace TemplateMethod.ConsoleApp
             Console.WriteLine("\nMaking coffee...");
             coffeeHook.PrepareRecipe();
 
+            var ducks = new Duck[]
+            {
+                new Duck("Daffy",8 ),
+                new Duck("Dewey",2 ),
+                new Duck("Howard",7 ),
+                new Duck("Louie",2 ),
+                new Duck("Donald",10 ),
+                new Duck("Huey",2 ),
+            };
+
+            Console.WriteLine("Before sorting:");
+            Display(ducks);
+
+            Array.Sort(ducks);
+
+            Console.WriteLine("\nAfter sorting:");
+            Display(ducks);
+
             Console.ReadKey();
+        }
+
+        private static void Display(Duck[] ducks)
+        {
+            foreach (var duck in ducks)
+            {
+                Console.WriteLine(duck);
+            }
         }
     }
 }
---
+++ TemplateMethod/TemplateMethod.Lib/Duck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod.Lib
+{
+    public class Duck : IComparable
+    {
+        public int Weight { get; private set; }
+        public string Name { get; private set; }
+
+        public Duck(string name, int weight)
+        {
+            Weight = weight;
+            Name = name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Duck otherDuck))
+                throw new ArgumentException("the argument was not a duck");
+
+            if (this.Weight < otherDuck.Weight)
+                return -1;
+            else if (this.Weight == otherDuck.Weight)
+                return 0;
+            else
+                return 1;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} weights {this.Weight}";
+        }
+    }
+}
